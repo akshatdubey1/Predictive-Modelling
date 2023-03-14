b3ff71bdab9ad181e75a58f6562468a996a1bb03
LovisaRut <lukkakrukka@hotmail.com>
2023-03-14 17:19:18 +0100

    Update CD_head_modal_a3.m

--- CD_head_modal_a3.m
+++ CD_head_modal_a3.m
@@ -16,7 +16,7 @@ freq1 = 40; % [Hz] eigenfreq lensassy;
 K02 = (2*pi*freq1)^2 * (M2+M1+M3);
 K12 = 0.2e6; % [N/m]
 K23 = 1.0e6; % [N/m]
-K23 = K12*((((M3)*(freq1*2*pi)^2))/(((M1)*(freq1*2*pi)^2)))
+%K23 = K12*((((M3)*(freq1*2*pi)^2))/(((M1)*(freq1*2*pi)^2)))
 
 K = [
       K12 -K12           0;
@@ -30,8 +30,8 @@ n = size(V);
 
 %test of plotting the mode shapes
 %mode1
-figure(1)
-V=V.*-1
+figure
+V=V.*-1;
 subplot(1,3,1)
 plot([V(1,3) 0],[1 1]); hold on
 plot([V(1,2) 0],[2 2]); hold on
@@ -39,6 +39,10 @@ plot([V(1,1) 0],[3 3]); hold on
 plot([0 0],[4 0])
 xlim([-50,50])
 ylim([0,4])
+yticks([1,2,3])
+yticklabels(["13 kHz - 1", "5 kHz - 2", "40 Hz - 3"])
+xticks([])
+title("Focus coil")
 subplot(1,3,2)
 plot([V(2,3) 0],[1 1]); hold on
 plot([V(2,2) 0],[2 2]); hold on
@@ -46,6 +50,9 @@ plot([V(2,1) 0],[3 3]); hold on
 plot([0 0],[4 0])
 xlim([-50,50])
 ylim([0,4])
+yticks([1,2,3])
+xticks([])
+title("Radial coil")
 subplot(1,3,3)
 plot([V(3,3) 0],[1 1]); hold on
 plot([V(3,2) 0],[2 2]); hold on
@@ -53,6 +60,9 @@ plot([V(3,1) 0],[3 3]); hold on
 plot([0 0],[4 0])
 xlim([-50,50])
 ylim([0,4])
+yticks([1,2,3])
+xticks([])
+title("Lens")
 
 
 
@@ -61,7 +71,7 @@ ylim([0,4])
  %    V(:,i) = V(:,i)/max(V(:,i));
  %end
 
-V=V.*-1
+V=V.*-1;
 % Eigenvalues to eigenfrequencies
 D = sqrt(D)/(2*pi);
 
@@ -87,12 +97,12 @@ for i=1:n(2)
 end
 
 % Plot response
-figure(2); 
+%figure(2); 
 %bode( mode(1), mode(2), mode(3), logspace(1,5,1000)*2*pi);
 %legend("mode 1", "mode 2", "mode 3")
 
 
-figure(3); 
+%figure(3); 
 %bode( mode(1), 'r', mode(2), 'r', mode(3), 'r', mode(1)+mode(2)+mode(3), 'k', logspace(1,5,1000)*2*pi);
 %legend("mode 1", "mode 2", "mode 3", "combined modes")
 
