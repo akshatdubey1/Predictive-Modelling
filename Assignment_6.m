5440c27bf598277a25a093f91f3a21be5235d54f
LovisaRut <lukkakrukka@hotmail.com>
2023-03-28 16:34:48 +0200

    Assignment 6 - updates
    Still something wrong with part 5

--- Assignment_6.m
+++ Assignment_6.m
@@ -1,34 +1,27 @@
 %%Assignment 6
 
+%Part 1
 M1=15  %Mass of the stage, kg
 K1=17e6  %Stiffness of stage, N/m
 C1=160  %Damping of stage, Ns/m
 I=0.1  %Inertia kgm2/rad
+x_stroke=250e-3; %stroke 
 
 
+%% 
+%Part 2
 
-
-%PID controller
-%w_bw=sqrt(K1/M1);
-%f_bw=w_bw/(2*pi)
-f_bw = 63; %Bandwidth of controller, Hz
-w_bw = 2*pi*f_bw;
-
-%Defining P value
-Kp = (2*pi*f_bw)^2*M1;
-
-%Defining D value
-f_d = f_bw/3;
-w_d = 2*pi*f_d;
-T_d = 1/w_d; 
-
-%Defining taming action (N)
-f_t = f_bw*3;
-w_t = 2*pi*f_t;
-T_t = 1/w_t;
-
-%Defining I value
-f_i = f_bw/10;
-w_i = 2*pi*f_i;
-T_i = 1/w_i;
+%Actuator block
+M1=5           %Mass, kg
+K1=17E6        %Stiffness, N/m
+C1=160         %Damping, Ns/m
+I1=0.025       %Moment of inertia, kgm2/rad
+%Mirror block
+M2= 10         %Mass, kg
+K2=22.5E6      %Stifness, N/m
+C2=150         %Damping, Ns/m
+I2=0.05        %Moment of inertia, kgm2/rad
+%Connection of the bodies
+K3=19E6        %Stiffness, N/m
+C3=75          %Damping, Ns/m
 
