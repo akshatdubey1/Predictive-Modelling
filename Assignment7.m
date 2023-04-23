6eb03eaf29a1793da51a3d4b2aab5c2ea461b2a5
LovisaRut <89640424+LovisaRut@users.noreply.github.com>
2023-04-23 14:01:15 +0200

    part 5 start

--- Assignment7.m
+++ Assignment7.m
@@ -8,13 +8,13 @@ F=40;                    % [N],  max fore per actuator
 %Model setup
 m2 = 5;                  %[kg], left mass, linear motor
 m3 = 10;                 %[kg], center mass, stage
-m4 = 5;                  %[kg], right mass, linear motor  
+m4 = 5;                  %[kg], right mass, linear motor    
 
 k_rot = 2e4;             %[Nm/rad],  rotational stiffness
 c_rot = 2;               %[Nms/rad], rotational damping
 
 I1 = 0.1;                %[kgm2], moment of inertia of the stage
-I2 = 0.02;                %[kgm2], moment of inertia of the stage motors
+I2 = 0.02;               %[kgm2], moment of inertia of the stage motors
 
 d1 = 25e-3;              %[m], distance from center of m3 to sensor
 d2 = 250e-3;             %[m], distance from middle of m3 to m2 and m4
@@ -26,4 +26,15 @@ f_bw = 100;              %[Hz], bandwidth of the controller
 
 [P, I, D, N, Kp] = PIDcode(f_bw,m1);
 
+
+%% Q12
+
+m5 = 5;                  %[kg], masses in part 5  
+
+k_rot_2 = 10^6;             %[Nm/rad],  rotational stiffness
+c_rot_2 = 5;               %[Nms/rad], rotational damping
+
+
+k_lin = 30e6;             %[Nm/rad],  rotational stiffness
+c_lin = 300;               %[Nms/rad], rotational damping
 
